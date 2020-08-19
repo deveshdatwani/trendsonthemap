aba28eac039beac82146d821e6532d1ce97d793e
deveshdatwani <datwanidevesh@gmail.com>
2020-08-20 04:56:06 +0530

    adding worker

--- create_world_map_trends.py
+++ create_world_map_trends.py
@@ -4,7 +4,11 @@ import json
 import requests
 import pandas as pd
 from find_trends import find_current_trends
+from apscheduler.schedulers.blocking import BlockingScheduler
 
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=16)
 def load_map_with_trends():
 
 	find_current_trends()
@@ -14,14 +18,12 @@ def load_map_with_trends():
 	lat = country_cordinates['lat']
 	lon = country_cordinates['lng']
 	city = country_cordinates['city']
+	trends = country_cordinates['trends']
+	#print('Marking cities')
 	for i, city in enumerate(city):
 		try:
-			folium.Marker([lat[i], lon[i]], popup = '<i>This is {}<i>'.format(city), tooltip = tooltip).add_to(m)
+			folium.Marker([lat[i], lon[i]], popup = '<i>{}<i>'.format(trends[i]), tooltip = tooltip).add_to(m)
 		except: pass
 	m.save('templates/trends_on_the_map.html')
 
 	return None
-
-if __name__ == '__main__':
-
-	load_map_with_trends()
--- find_trends.py
+++ find_trends.py
@@ -3,20 +3,51 @@ from boto.s3.connection import S3Connection
 import requests
 import pandas as pd
 
-#bearer_token = S3Connection(os.environ['BEARER_TOKEN'])
-
 def find_current_trends():
 
+	bearer_token = S3Connection(os.environ['BEARER_TOKEN'])
+	inter  = S3Connection(os.environ['INTER'])
+	split_end = inter * 70
+	split_start = split_end - 70
 	url = 'https://api.twitter.com/1.1/trends/available.json'
 	bearer = os.environ['BEARER_TOKEN']
 	response = requests.get(url=url, headers = {'authorization': 'Bearer ' + bearer})
-	worldwide_trending = response.json()
+	response = response.json()
+	trending_cities = []
+	#print('Available trends acquired')
 
+	for trend in response:
+		trending_cities.append([trend['name'], trend['country'], trend['woeid']])
+
+	trending_cities_df = pd.DataFrame(trending_cities, columns=['city','country','woeid'])
 	city_coordinates = pd.read_csv('worldcities.csv')
-	trending_woeid = [[trend['name'], trend['country'], trend['woeid']] for trend in worldwide_trending]
-	trending_woeid = pd.DataFrame(trending_woeid, columns = ['city', 'country', 'woeid'])
+	city_coordinates = city_coordinates[['city','country','lat','lng']]
+	trending_cities_df = trending_cities_df.merge(city_coordinates,on=['country','city'])
+	woeids = trending_cities_df['woeid'].values
+	trends_in_woeids = []
+	url2 = 'https://api.twitter.com/1.1/trends/place.json'
+	woeids = woeids[split_start:split_end]
+	
+	for woeid in woeids:
+		param = {'id':woeid}
+		response = requests.get(url = url2, headers = {'authorization': 'Bearer ' + bearer}, params = param).json()
+		#print('Acquiring trends for {}'.format(woeid))
+		trends_in_woeids.append(response)	
 
-	city_coordinates = city_coordinates[['city', 'lat', 'lng', 'country']]
-	trending_cities = trending_woeid.merge(city_coordinates, on=['country','city'])
+	3print('All trends acquired')
+	trending_seventy_in_woeids = trends_in_woeids[split_start:split_end]
+	l1 = []
 
-	trending_cities.to_csv('trending_cities.csv')
+	for i in trending_seventy_in_woeids:
+		l2 = []
+		for j in i[0]['trends']:
+			l2.append(j['name'])
+		l1.append(l2)
+ 	
+	Strending_seventy = trending_cities_df[split_start:split_end]
+	trending_seventy.insert(5, 'trends', l1)
+	trending_seventy.to_csv('trending_cities.csv')
+	if S3Connection(os.environ['INTER']) > 4:
+		S3Connection(os.environ['INTER']) = 1
+	else:
+		S3Connection(os.environ['INTER']) += 1
