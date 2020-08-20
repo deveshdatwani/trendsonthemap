8f310af794cf93d22a1603933c0d9ae8a24d291f
deveshdatwani <datwanidevesh@gmail.com>
2020-08-20 18:00:52 +0530

    15 minute window

--- create_world_map_trends.py
+++ create_world_map_trends.py
@@ -8,7 +8,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=2)
+@sched.scheduled_job('interval', minutes=15)
 def load_map_with_trends():
 
 	find_current_trends()
