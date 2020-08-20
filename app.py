0ed3dfda5391a26fabfc59f3ee4d3b6ae874067c
deveshdatwani <datwanidevesh@gmail.com>
2020-08-21 00:46:57 +0530

    auto reload

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.config['DEBUG'] = True
 
 @app.route('/', methods=('GET','POST'))
 def main_page():
