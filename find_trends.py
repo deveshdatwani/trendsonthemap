df6a6cae647c6f1d142e8f5d83addcbde659a356
deveshdatwani <datwanidevesh@gmail.com>
2020-08-23 07:44:11 +0530

    working version now going live

--- find_trends.py
+++ find_trends.py
@@ -3,20 +3,29 @@ import requests
 import pandas as pd
 from time import sleep
 import mysql.connector
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-def find_current_trends():
+sched = BlockingScheduler()
 
+@sched.scheduled_job('interval', minutes=15)
+def find_trends():
+    
     bearer = os.environ['BEARER_TOKEN']
     print('trying to connect to server')
     connector = mysql.connector.connect(user='be5852720363b4', password='936fcbd3', host='us-cdbr-east-02.cleardb.com', database='heroku_4ac3cade96b682b')
-    
+
     if connector.is_connected():    
         cursor = connector.cursor(buffered=True)
         query = 'SELECT * FROM iterator WHERE holder = "it_holder" '
         cursor.execute(query)
         response = cursor.fetchone()
         i = response[1]
-        response = response[1] + 1
+        
+        if i > 4:
+            response = 1
+        else:
+            response = response[1] + 1
+        
         print('This is update iterator {}'.format(response))
         second_query = "UPDATE iterator SET it = {} WHERE holder = 'it_holder' ".format(response)
         cursor.execute(second_query)
@@ -27,12 +36,12 @@ def find_current_trends():
         print('iterator token accessed and updated. Fetched trendingcities')
         print(i)
 
-    split_end = i * 5
-    split_start = split_end - 5
+    split_end = i * 70
+    split_start = split_end - 70
 
     trends = [x[6] for x in response]
     woeids = [x[3] for x in response] 
-    #print(woeids)
+    woeids_copy = woeids
     woeids = woeids[split_start:split_end]
     trends_in_woeids = []
     url = 'https://api.twitter.com/1.1/trends/place.json'
@@ -42,25 +51,33 @@ def find_current_trends():
         param = {'id' : woeid}
         response = requests.get(url = url, headers = {'authorization': 'Bearer ' + bearer}, params = param).json()
         l1 = []
-        #print(response)
-        for i in response[0]['trends']:
-            l1.append(i['name'])
-        l2.append(l1)
+        
+        try:
+            for i in response[0]['trends']:
+                l1.append(i['name'])
+            l2.append(l1)
 
     print('All trends acquired')
 
-    trends[split_start:split_end] = l2
-    #print(trends)
+    if len(l2) != 0:
+        trends[split_start:split_end] = l2
+    
+    for num, i in enumerate(trends):
+        if type(i) == list:
+             trends[num] = ' '.join(i)
+
+    trends = tuple(trends)
     connector = mysql.connector.connect(user='be5852720363b4', password='936fcbd3', host='us-cdbr-east-02.cleardb.com', database='heroku_4ac3cade96b682b')
     cursor = connector.cursor(buffered=True)
-    values = [str(item) for item in trends]
-    #values = ','.join(values)
-    #fourth_query = 'UPDATE trendsincities SET trends= ? '.
-    cursor.execute('UPDATE trendsincities SET trends = "{}"'.format(values))
-    connector.commit()
-    print(cursor._last_executed)
+
+    for i, trend in enumerate(trends):
+        cursor.execute('UPDATE trendsincities SET trends = %s WHERE woeid = %s', (trend, woeids_copy[i]))
+
+        connector.commit()
     print('Done')
     cursor.close()
     connector.close()
 
-find_current_trends()
+    return None
+
+sched.start()
