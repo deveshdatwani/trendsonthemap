d9cc14ac9dc11434773a157dd750bf511824321f
Devesh <datwanidevesh@gmail.com>
2020-09-14 16:38:51 +0530

    using jinja templates

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import pandas as pd
 import mysql.connector
 import folium
+from jinja2 import PackageLoader, select_autoescape, Environment, Template
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
@@ -12,11 +13,11 @@ def main_page():
 	if request.method == 'GET':
 
 		tooltip = 'See Trends'
-	
+
 		connector = mysql.connector.connect(user='be5852720363b4', password='936fcbd3', host='us-cdbr-east-02.cleardb.com', database='heroku_4ac3cade96b682b')
 		m = folium.Map(tiles='Stamen Terrain',  zoom_start=3, location=[20.76, 79])
 
-		if connector.is_connected():	
+		if connector.is_connected():
 			cursor = connector.cursor()
 			query = 'SELECT * FROM trendsincities'
 			cursor.execute(query)
@@ -25,7 +26,18 @@ def main_page():
 			connector.close()
 
 			for i, city in enumerate(response):
-								
-				folium.Marker([round(city[4],2), round(city[5],2)], popup = '<meta charset="UTF-8"><i>{}<i>'.format(city[6].split(' ')[:10]), tooltip = tooltip).add_to(m)
-				
-	return m._repr_html_()
+
+				try:
+
+					folium.Marker([round(city[4],2), round(city[5],2)], popup = '<i>{}<i>'.format(city[6]), tooltip = tooltip).add_to(m)
+
+				except Exception as e: print(e)
+
+		final_display = m._repr_html_()
+
+		env = Environment( loader=PackageLoader('app', 'templates'), autoescape=select_autoescape(['html', 'xml']) )
+		template = env.get_template('trends.html')
+		map = template.render(map=final_display)
+
+	return map
+
